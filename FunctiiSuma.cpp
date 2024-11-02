c84bc44c4ec8e311874d35763e5b06df4818dc53
Erica114 <ostrovan.erica140@icloud.com>
2024-11-02 17:57:20 +0200

    Create FunctiiSuma.cpp

---
+++ FunctiiSuma.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int Suma(int a, int b){
+return a + b;
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    cout << Suma(n, m);
+    return 0;
+}
